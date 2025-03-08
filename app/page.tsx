98c3e8ece89ac57be62f80a9dfca8d19ee8db826
dev-conquer-codes <rakshit2871@gmail.com>
2025-03-08 11:48:44 +0530

    fourth

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,18 @@
-"use client";
-import { useEffect, useState } from "react";
+'use client'
 import { fetchAccessToken } from "hume";
 import ClientComponent from "./_components/ClientComponent";
 
-export default function Page() {
-  const [accessToken, setAccessToken] = useState<string | null>(null);
 
-  useEffect(() => {
-    const getToken = async () => {
-      try {
-        const token = await fetchAccessToken({
-          apiKey: String(process.env.NEXT_PUBLIC_HUME_API_KEY),
-          secretKey: String(process.env.NEXT_PUBLIC_HUME_SECRET_KEY),
-        });
-        setAccessToken(token);
-      } catch (error) {
-        console.error("Error fetching access token", error);
-      }
-    };
-
-    getToken();
-  }, []);
+export default async function Page() {
+  const accessToken = await fetchAccessToken({
+    apiKey: String(process.env.HUME_API_KEY),
+    secretKey: String(process.env.HUME_SECRET_KEY),
+  });
 
   if (!accessToken) {
-    return <p>Loading...</p>;
+    throw new Error();
   }
+ 
 
   return <ClientComponent accessToken={accessToken} />;
 }
