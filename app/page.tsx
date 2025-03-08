6bb1407b4852c66bfc8140d44309b548c29f746a
dev-conquer-codes <rakshit2871@gmail.com>
2025-03-08 12:10:14 +0530

    eight

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,19 @@
-'use client';
 
-import { useEffect, useState } from 'react';
-import ClientComponent from './_components/ClientComponent';
 
-export const dynamic = 'force-dynamic'; // ✅ Disable static generation
+import { fetchAccessToken } from "hume";
+import ClientComponent from "./_components/ClientComponent";
 
-export default function Page() {
-  const [accessToken, setAccessToken] = useState<string | null>(null);
 
-  useEffect(() => {
-    const fetchToken = async () => {
-      try {
-        const response = await fetch('/api/getAccessToken');
-        if (!response.ok) throw new Error('Failed to fetch token');
+export default async function Page() {
+  const accessToken = await fetchAccessToken({
+    apiKey: String(process.env.HUME_API_KEY),
+    secretKey: String(process.env.HUME_SECRET_KEY),
+  });
 
-        const data = await response.json();
-        setAccessToken(data.accessToken);
-      } catch (error) {
-        console.error('Error fetching token:', error);
-      }
-    };
-
-    fetchToken();
-  }, []);
-
-  if (!accessToken) return <div>Loading...</div>;
+  if (!accessToken) {
+    throw new Error();
+  }
+ 
 
   return <ClientComponent accessToken={accessToken} />;
 }
